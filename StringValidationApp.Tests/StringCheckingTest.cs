b70ea99a201a53f6e28404b168861ab6842ad308
feshu <feshu@LAPTOP>
2022-12-21 15:44:09 +0300

    Добавьте файлы проекта.

---
+++ StringValidationApp.Tests/StringCheckingTest.cs
@@ -0,0 +1,45 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringValidationApp.Tests
+{
+    [TestClass]
+    public class StringCheckingTest
+    {
+        [TestMethod()]
+        public void StringChecking_EmptyString_ReturnTrue()
+        {
+            string inputString = string.Empty;
+
+            bool outputValue = Program.StringChecking(inputString);
+
+            Assert.IsTrue(outputValue);
+        }
+        [TestMethod()]
+        public void StringChecking_StringWithoutBrackets_ReturnTrue()
+        {
+            string inputString = string.Empty;
+
+            bool outputValue = Program.StringChecking(inputString);
+
+            Assert.IsTrue(outputValue);
+        }
+        [TestMethod()]
+        public void StringChecking_StringWithCorrectBrackets_ReturnTrue()
+        {
+            string inputString = "Этот текст (строка) предназначен для тестирования [бы{стр}ой] проверки вхождения скобок {любых [из трех видов]}";
+
+            bool outputValue = Program.StringChecking(inputString);
+
+            Assert.IsTrue(outputValue);
+        }
+        [TestMethod()]
+        public void StringChecking_StringWithoutCorrectBrackets_ReturnFalse()
+        {
+            string inputString = "[ ] ({}) ) [])";
+
+            bool outputValue = Program.StringChecking(inputString);
+
+            Assert.IsFalse(outputValue);
+        }
+    }
+}
---
+++ StringValidationApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringValidationApp
+{
+    public class Program
+    {
+        public static bool StringChecking(string inputText)
+        {
+            var symbols = "[]{}()";
+            var Cache = new Stack<char>();
+
+            foreach(char item in inputText){
+                if("{([".Contains(item))
+                    Cache.Push(item);
+                else if ("}])".Contains(item))
+                {
+                    if (Cache.Count == 0)
+                        return false;
+
+                    var lastItem = Cache.Pop();
+                    var index = symbols.IndexOf(lastItem);
+
+                    if (symbols[index+1] != item)
+                        return false;
+                }
+            }
+
+            return (Cache.Count == 0) ? true : false;
+        }
+        public static int Main(string[] args)
+        {
+            string inputText;
+            if (args.Length!=0)
+                inputText = args[0];
+            else
+            {
+                Console.WriteLine("Введите строку для анализа расположения ковычек: ");
+                inputText = Console.ReadLine();
+            }
+
+            var result = StringChecking(inputText);
+            Console.WriteLine($"В строке {(result?"не ":string.Empty)}найдены ошибки в расположении и количестве ковычек.");
+            
+            return result?0:1;
+        }
+    }
+}
